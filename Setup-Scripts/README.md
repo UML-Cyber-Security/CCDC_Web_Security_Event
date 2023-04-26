6c8d6bde58f29996412e08e4102173f99c61b2f1
DaintyJet <Matther_harper30@aol.com>
2023-04-26 09:39:25 -0400

    Created Files
    Created the base structure of the repository. The files are empty and have names for order - we can add to the names of the task files for clarity.

---
+++ Setup-Scripts/README.md
@@ -0,0 +1,1 @@
+This is a place for the scripts used to setup and configure the (basic) infrastructure
---
+++ Website-Container/build.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+# Some script to build and push the created container to a docker hub repository

a56d75c0208054ef114bf1d685d30b3e9471cd7c
DaintyJet <Matther_harper30@aol.com>
2023-05-12 16:57:34 -0400

    Update

--- Setup-Scripts/TERAFORM/digitalocean/03-droplets.tf
+++ Setup-Scripts/TERAFORM/digitalocean/03-droplets.tf
@@ -5,5 +5,5 @@ resource "digitalocean_droplet" "vms" {
   size     = "s-1vcpu-512mb-10gb"
   image    = "ubuntu-22-04-x64"
   region   = "nyc1"
-  ssh_keys = ["33:c5:bc:b9:8c:90:4b:55:1a:fa:f1:d8:63:e4:6c:24"]
+  ssh_keys = []
 }

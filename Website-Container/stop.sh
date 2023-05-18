ad66c64faf649c97811e7c1460bad2b68ea52451
ChrisM09 <53067353+ChrisM09@users.noreply.github.com>
2023-05-18 15:54:49 -0400

    Update stop.sh
    Commented volatile command. Should remove entirely.

--- Website-Container/stop.sh
+++ Website-Container/stop.sh
@@ -1,3 +1,3 @@
 docker stop database website api-server
 #Idk man it is handy to have the command to delete the containers. They aren't changing or anything
-docker rm -f $(docker ps -aq)
+#docker rm -f $(docker ps -aq)
